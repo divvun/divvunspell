2e710cba8b8973cc8096267f586d6e07b5ab513c
Brendan Molloy <brendan@technocreatives.com>
2019-11-19 21:19:06 +0100

    Try fixing FFI for word bound indices

--- divvunspell/src/ffi/mod.rs
+++ divvunspell/src/ffi/mod.rs
@@ -250,38 +250,29 @@ use std::sync::Arc;
 use crate::tokenizer::word::WordBoundIndices;
 use crate::tokenizer::Tokenize;
 
-pub struct CWordBoundIndices {
-    string: String,
-    iterator: WordBoundIndices<'static>,
-}
-
 #[no_mangle]
-pub extern "C" fn word_bound_indices(utf8_string: *const c_char) -> *mut CWordBoundIndices {
+pub extern "C" fn divvun_word_bound_indices(utf8_string: *const c_char) -> *mut WordBoundIndices<'static> {
     let c_str = unsafe { CStr::from_ptr(utf8_string) };
-    let string = c_str.to_str().unwrap().to_string();
-
-    let mut thing = CWordBoundIndices {
-        string,
-        iterator: unsafe { std::mem::uninitialized() },
-    };
-
-    thing.iterator = unsafe { std::mem::transmute(thing.string.word_bound_indices()) };
-    Box::into_raw(Box::new(thing))
+    let string = c_str.to_str().unwrap();
+    let iterator = string.word_bound_indices();
+    Box::into_raw(Box::new(iterator)) as *mut _
 }
 
 #[no_mangle]
-pub extern "C" fn word_bound_indices_next(
-    handle: *mut CWordBoundIndices,
+pub extern "C" fn divvun_word_bound_indices_next(
+    iterator: *mut WordBoundIndices<'static>,
     out_index: *mut u64,
-    out_string: *mut *mut c_char,
+    out_string: *mut *const c_char,
 ) -> u8 {
-    let handle = unsafe { &mut *handle };
+    let iterator = unsafe { &mut *iterator };
 
-    match handle.iterator.next() {
+    match iterator.next() {
         Some((index, word)) => {
-            unsafe { *out_index = index as u64 };
             let c_word = CString::new(word).unwrap();
-            unsafe { *out_string = c_word.into_raw() };
+            unsafe {
+                *out_index = index as u64;
+                *out_string = c_word.into_raw();
+            }
             1
         }
         None => 0,
@@ -289,7 +280,7 @@ pub extern "C" fn word_bound_indices_next(
 }
 
 #[no_mangle]
-pub extern "C" fn word_bound_indices_free(handle: *mut CWordBoundIndices) {
+pub extern "C" fn divvun_word_bound_indices_free(handle: *mut WordBoundIndices) {
     unsafe { Box::from_raw(handle) };
 }
 
