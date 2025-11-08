725136ee823916da747477fc575e574c82dd46a2
Brendan Molloy <brendan@necessary.nu>
2025-11-08 19:52:56 +0100

    Add Java JNI

--- ffi/examples/spellcheck.c
+++ ffi/examples/spellcheck.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../include/divvunspell_cffi.h"
+#include "../include/divvun_fst.h"
 
 // Error handling
 static char* last_error = NULL;
